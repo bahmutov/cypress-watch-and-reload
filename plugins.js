7dd186a24839307faf77ff4a2f14237f52573cf8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-05 22:50:06 -0500

    fix: use working directory

--- plugins.js
+++ plugins.js
@@ -1,12 +1,15 @@
 const WebSocket = require('ws')
+const { join } = require('path')
 
 // https://github.com/websockets/ws#simple-server
+// create socket even if not watching files to avoid
+// tripping up client trying to connect
 const wss = new WebSocket.Server({ port: 8765 })
 let client // future Cypress client
 
 // watch files using chokidar
 const chokidar = require('chokidar')
-const cypressJson = require('./cypress.json')
+const cypressJson = require(join(process.cwd(), 'cypress.json'))
 const options = cypressJson['cypress-watch-and-reload']
 if (options && typeof options.watch === 'string') {
   console.log('will watch "%s"', options.watch)
@@ -28,4 +31,9 @@ if (options && typeof options.watch === 'string') {
       }
     })
   })
+} else {
+  console.log(
+    'nothing to watch. Use cypress.json to set "cypress-watch-and-reload" object'
+  )
+  console.log('see https://github.com/bahmutov/cypress-watch-and-reload#use')
 }
