bb5b6cc5165b5fdad6f7bc7c5a42d9cb395892f7
Damien Cassou <damien@cassou.me>
2021-01-06 22:48:59 +0100

    fix: Wrap promise to avoid a Cypress warning (#60) (#83)
    This uses the recommendation in
    https://github.com/cypress-io/cypress/issues/3172.

--- support.js
+++ support.js
@@ -3,28 +3,31 @@
 const waitUntil = require('async-wait-until')
 const ws = new WebSocket('ws://localhost:8765')
 
-beforeEach(async () => {
-  await waitUntil(() => ws.readyState === WebSocket.OPEN, 2000, 50);
-
-  ws.onmessage = ev => {
-    console.log('message from OS')
-    console.log(ev)
-    if (ev.type === 'message' && ev.data) {
-      try {
-        const data = JSON.parse(ev.data)
-        if (data.command === 'reload' && data.filename) {
-          console.log(
-            'reloading Cypress because "%s" has changed',
-            data.filename
-          )
-          window.top.document.querySelector('.reporter .restart').click()
+beforeEach(() => {
+  cy.wrap(
+    waitUntil(() => ws.readyState === WebSocket.OPEN, 2000, 50),
+    { log: false }
+  ).then(() => {
+    ws.onmessage = ev => {
+      console.log('message from OS')
+      console.log(ev)
+      if (ev.type === 'message' && ev.data) {
+        try {
+          const data = JSON.parse(ev.data)
+          if (data.command === 'reload' && data.filename) {
+            console.log(
+              'reloading Cypress because "%s" has changed',
+              data.filename
+            )
+            window.top.document.querySelector('.reporter .restart').click()
+          }
+        } catch (e) {
+          console.error('Could not parse message from plugin')
+          console.error(e.message)
+          console.error('original text')
+          console.error(ev.data)
         }
-      } catch (e) {
-        console.error('Could not parse message from plugin')
-        console.error(e.message)
-        console.error('original text')
-        console.error(ev.data)
       }
     }
-  }
+  })
 })
