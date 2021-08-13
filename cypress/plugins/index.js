2b845952c00ea6eb2842566746d169f92d2840b8
Kalin Chernev <kalin.chernev@gmail.com>
2021-08-13 15:02:49 +0200

    Allow cypress.json config from custom folder

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,5 @@
-require('../../plugins')
-
 module.exports = (on, config) => {
+  require('../../plugins')(config)
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
